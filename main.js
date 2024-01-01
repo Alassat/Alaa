a560ed06f714c7126536de4998427415f88e874a
Alassat <138383372+Alassat@users.noreply.github.com>
2024-01-01 23:47:08 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("running");
+
+
+function search() {
+    let searcbar = document.querySelector('.search-input').value.toUpperCase();
+    let booklist = document.querySelector('.books');
+    let book = document.querySelectorAll('.one');
+    let bookName = document.getElementsByTagName('a');
+    for (let i = 0; i < bookName.length; i++) {
+        if (bookName[i].innerHTML.toUpperCase().indexOf(searcbar) >= 0) {
+            book[i].style.display = "";
+        } else {
+            book[i].style.display = "none";
+        }
+    }
+
+}
